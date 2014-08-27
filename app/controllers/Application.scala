b8f4396b41807093338b671074a437eccff19c8e
Rafa Paradela <rafa.p@47deg.com>
2014-08-27 11:30:02 +0200

    Second change

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready. v.rp"))
+    Ok(views.html.index("Your new application is ready. v.rp 2"))
   }
 
 }
